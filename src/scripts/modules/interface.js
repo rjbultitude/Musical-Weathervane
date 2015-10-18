d20ae1c9c0b0a839560aa648623b4eac6a3d35dc
rjbultitude <richard.bultitude@gmail.com>
2015-10-18 19:41:48 +0100

    remove unused vars

--- src/scripts/modules/interface.js
+++ src/scripts/modules/interface.js
@@ -6,7 +6,6 @@ require('../libs/p5.sound');
 
 module.exports = function() {
 	var myP5 = new P5(function(sketch) {
-		var organC2 = null;
 		var numNotes = 0;
 		var organNotes = [];
 
@@ -33,12 +32,13 @@ module.exports = function() {
 				//bearing
 				var thisBearing = locationsData[num].bearing;
 				var volume = sketch.map(thisBearing, 0, 100, 0, 1);
-  				var nVolume = sketch.constrain(volume, 0.5, 1);
+  				volume = sketch.constrain(volume, 0.5, 1);
   				
   				//speed
   				var thisSpeed = locationsData[num].bearing;
 				var speed = sketch.map(thisSpeed, 0.1, 100, 0, 2);
-				var nSpeed = sketch.constrain(speed, 0.01, 4);
+				speed = sketch.constrain(speed, 0.01, 4);
+				console.log('speed', speed);
   				organNotes[i].amp(volume);
 				organNotes[i].rate(speed);
 				num++;
@@ -51,15 +51,7 @@ module.exports = function() {
 			sketch.noStroke();
 			sketch.fill(255);
 			sketch.rect(sketch.mouseX, sketch.mouseY, 10, 10);
-
-			// for (var i = numNotes - 1; i >= 0; i--) {
-				
-			// };
 			
-			/// Set the volume to a range between 0 and 1.0
-
-			// Set the rate to a range between 0.1 and 4
-			// Changing the rate alters the pitch
 		};
 
 	}, 'canvas-container');
